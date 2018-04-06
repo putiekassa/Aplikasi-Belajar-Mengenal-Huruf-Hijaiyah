63ff91e5016fea06c231ea33c76b839bfcbe748d
dessyamaliyaa <dessy.amaliyaa@gmail.com>
2018-04-06 15:48:24 +0700

    mengenal huruf hijaiyah

---
+++ app/src/main/java/com/example/tumpex/belajarmengaji/BelajarActivity.java
@@ -0,0 +1,13 @@
+package com.example.tumpex.belajarmengaji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BelajarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belajar);
+    }
+}
---
+++ app/src/main/java/com/example/tumpex/belajarmengaji/ngajiActivity.java
@@ -0,0 +1,13 @@
+package com.example.tumpex.belajarmengaji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ngajiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngaji);
+    }
+}
